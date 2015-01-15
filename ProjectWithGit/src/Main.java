d9664cf67936e9b0641a732f4447268a88743e05
behshad <behshad@localhost>
2015-01-15 13:18:18 -0500

    just testing to make sure Im online. Behshad

--- ProjectWithGit/src/Main.java
+++ ProjectWithGit/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[]args){
 		System.out.println("Fun with Git");
 		System.out.println("This is my Change");
+		System.out.println("Behshad access");
 		
 	}
 }
