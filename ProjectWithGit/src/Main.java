4cc769a1e561f0a6a2aae8f6c4ef6f9ceed5f3e3
azkevin <karindaeng@gmail.com>
2015-01-19 02:37:40 -0500

    I have committed

--- ProjectWithGit/src/Main.java
+++ ProjectWithGit/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
 		System.out.println("Fun with Git");
 		System.out.println("This is my Change");
 		System.out.println("Behshad access");
-		
+		System.out.println("Kevin access");
 	}
 }
