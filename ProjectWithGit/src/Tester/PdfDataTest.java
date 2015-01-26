74acf1af97ebcfb9eea61d1a987ca95561e7a375
Abasifreke <jamesabasifreke@yahoo.com>
2015-01-26 13:13:10 -0500

    Trying to commit a Junit test Class. It works, it seems.

---
+++ ProjectWithGit/src/Tester/PdfDataTest.java
@@ -0,0 +1,14 @@
+package Tester;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PdfDataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
