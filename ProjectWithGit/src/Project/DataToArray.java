9cf75a2af5c6d0b0fef7d6e90b60a1a0bcb17486
bsebt <bsebt@my.yorku.ca>
2015-04-08 16:47:26 -0400

    reading lines properly.

--- ProjectWithGit/src/Project/DataToArray.java
+++ ProjectWithGit/src/Project/DataToArray.java
@@ -20,26 +20,26 @@ public class DataToArray {
 	public static String Title = "NO TITLE";
 	public static String SubTitle = "NO SUBTITLE";
 	public static float Spacing = 8.0f;
-	public static String correctLine = "^( |[0-9]|\\|)([0-9]|<|>|s|h|x|\\||\\*|\\-|p| |\\^|g|\\[|\\]|\\(|\\)|\\=|\\\\|\\/|S)+( |[0-9]|\\|)";
+	public static String correctLine = "^( |[0-9]|\\||[A-Z])([0-9]|<|>|s|h|x|\\||\\*|\\-|p| |\\^|g|\\[|\\]|\\(|\\)|\\=|\\\\|\\/|S)+( |[0-9]|\\|)";
 	//public static String starter = "([0-9]| |\\|)";
 
 	private static ArrayList<char[][]> newchars = new ArrayList<char[][]>();
 
 	private static int col;
 
-//	public static ArrayList<char[][]> TrimElement(ArrayList<char[][]> uncutArray) 
-//	{
-//		int actualLength = 0;
-//		ArrayList<char[][]> newCharList = new ArrayList<>();
-//		char[][] newElement = 
-//		for (int p = 0; p < chars.size(); p++) {
-//			for (int j=0;j<chars.get(p).length; j++){
-//				for(int i =0; i< chars.get(p)[j].length;i++)
-//					if()
-//					System.out.println((chars.get(p))[j][i]);
-//			}
-//		}
-//	}
+	//	public static ArrayList<char[][]> TrimElement(ArrayList<char[][]> uncutArray) 
+	//	{
+	//		int actualLength = 0;
+	//		ArrayList<char[][]> newCharList = new ArrayList<>();
+	//		char[][] newElement = 
+	//		for (int p = 0; p < chars.size(); p++) {
+	//			for (int j=0;j<chars.get(p).length; j++){
+	//				for(int i =0; i< chars.get(p)[j].length;i++)
+	//					if()
+	//					System.out.println((chars.get(p))[j][i]);
+	//			}
+	//		}
+	//	}
 
 	//		
 	//		for (int a = 0; a < element[0].length; a++) // Go through the full
@@ -62,10 +62,10 @@ public class DataToArray {
 	//				}
 	//			}
 	//		}
-//	return newestElement;
-//}
+	//	return newestElement;
+	//}
 
- 
+
 	public static ArrayList<char[][]> textToArray(File[] source)
 			throws DocumentException, IOException {
 		lines = new ArrayList<String>();
@@ -83,7 +83,7 @@ public class DataToArray {
 			name = source[i].getPath();
 			input = new BufferedReader(new FileReader(name));
 			while (null != (line = input.readLine())) {
-				//line = line.trim();
+				line = line.trim();
 				if (line.contains("SUBTITLE")) {
 					SubTitle = line.substring(line.indexOf('=') + 1,
 							line.length());
@@ -97,14 +97,15 @@ public class DataToArray {
 				else if (line.trim().length() == 0) {
 					continue;
 				}
-				else if(line.trim().length() < 8){ //new 
+				else if(line.trim().length() < 5){ //new 
 					continue;
 				}
-				else if (line.charAt(0) == ' '){ //new
-					continue;
-				}
-				else if (line.trim().substring(0, line.lastIndexOf('|') + 1).matches(correctLine) && !line.trim().substring(0, line.lastIndexOf('|') + 1).contains("  |")) {
-					
+				//				else if (line.charAt(0) == ' '){ //new
+				//					continue;
+				//				}
+				//	line.trim().substring(0, line.lastIndexOf('|') + 1).matches(correctLine) && !line.trim().substring(0, line.lastIndexOf('|') + 1).contains("  |")
+				else if (line.matches(correctLine)) {
+
 					if((line.charAt(0)+"").matches("[0-9]")){
 						//line = line.replace(line.charAt(0), '|');
 						line = "|" + line.substring(1);
@@ -122,15 +123,22 @@ public class DataToArray {
 			}
 			input.close();
 		}
-		
-		
+		//		for(int i = 0; i < lines.size(); i++)
+		//		{	if(i%6 == 0){
+		//			System.out.println();
+		//		}
+		//		System.out.println(lines.get(i));
+		//		}
+		lines = ProperLines(lines);
+
+//
 //		for(int i = 0; i < lines.size(); i++)
 //		{	if(i%6 == 0){
 //			System.out.println();
 //		}
-//			System.out.println(lines.get(i));
+//		System.out.println(lines.get(i));
 //		}
-		
+
 		ArrayList<String>lines1 = new ArrayList<String>();
 		String lastLine = "";
 		for(int i=0; i<lines.size() ; i++){
@@ -146,24 +154,24 @@ public class DataToArray {
 		}
 		lines1 = addDummyLines(lines1, lastLine);
 		for(int i=0; i<lines1.size() ; i=i+6){
-		//	System.out.println(i);
+			//	System.out.println(i);
 			for(int j=0;j<lines1.get(i).length();j++){
 				if((lines1.get(i).charAt(j)+"").matches("[0-9]")){
 					if(lines1.get(i).charAt(j-1) == '|' && lines1.get(i+1).charAt(j) == '|'){
 						//System.out.println("ok");
 					}else if(lines1.get(i+1).charAt(j) == '|'){
-					lines1.set(i, lines1.get(i).replaceFirst(lines1.get(i).charAt(j)+"", "|"));
+						lines1.set(i, lines1.get(i).replaceFirst(lines1.get(i).charAt(j)+"", "|"));
 					}		
 				}
 			}
 		}
-//		for(int i = 0; i < lines1.size(); i++)
-//		{	if(i%6 == 0){
-//			System.out.println();
-//		}
-//			System.out.println(lines1.get(i));
-//		}
-		
+		//		for(int i = 0; i < lines1.size(); i++)
+		//		{	if(i%6 == 0){
+		//			System.out.println();
+		//		}
+		//			System.out.println(lines1.get(i));
+		//		}
+
 		for(int i=0; i<lines1.size() ; i=i+6){
 			int min=lines1.get(i).length();
 			if(!((lines1.get(i).length() == lines1.get(i+1).length()) && (lines1.get(i+2).length() == lines1.get(i+3).length()) && (lines1.get(i+4).length()==lines1.get(i+5).length()))){
@@ -180,14 +188,14 @@ public class DataToArray {
 				}
 			}	
 		}
-		
+
 		ArrayList<String> lines2 = new ArrayList<String>();
 		for(int i=0;i<lines1.size();i++){
-//			System.out.println(lines1.get(i).indexOf('|', 2));
-//			System.out.println(lines1.get(i).lastIndexOf('|'));
-//			System.out.println(lines1.get(i).indexOf('|', 3));
-//			System.out.println(lines1.get(i).lastIndexOf('|')-1);
-//			System.out.println(lines1.get(i).length());
+			//			System.out.println(lines1.get(i).indexOf('|', 2));
+			//			System.out.println(lines1.get(i).lastIndexOf('|'));
+			//			System.out.println(lines1.get(i).indexOf('|', 3));
+			//			System.out.println(lines1.get(i).lastIndexOf('|')-1);
+			//			System.out.println(lines1.get(i).length());
 			while((lines1.get(i).indexOf('|', 2)) != (lines1.get(i).length()-1) && (lines1.get(i).indexOf('|', 2))!= (lines1.get(i).length()-2)){
 				for(int j=i;j<i+6;j++){
 					//System.out.println(lines1.get(j).substring(0,lines1.get(j).indexOf('|', 2)) + "|");
@@ -195,32 +203,32 @@ public class DataToArray {
 					lines1.set(j, lines1.get(j).substring(lines1.get(j).indexOf('|', lines1.get(j).indexOf('|', 2))));
 				}
 			}
-				lines2.add(lines1.get(i));
+			lines2.add(lines1.get(i));
 		}
-		
-		
-		for(int i = 0; i < lines2.size(); i++)
-		{	if(i%6 == 0){
-			System.out.println();
-		}
-			System.out.println(lines2.get(i));
-		}
-		
+
+
+		//		for(int i = 0; i < lines2.size(); i++)
+		//		{	if(i%6 == 0){
+		//			System.out.println();
+		//		}
+		//			System.out.println(lines2.get(i));
+		//		}
+
 		int temp = 0;
 		for (int z = 0; z < lines2.size(); z = z + 6) {
 			if(lines2.size()-temp >= 6)
 			{
 				c = new char[6][];
 			}
-//			else
-//			{
-//				c = new char[lines2.size()-temp][];
-//			}
+			//			else
+			//			{
+			//				c = new char[lines2.size()-temp][];
+			//			}
 			//temp++;
 			for (int i = 0; i < 6 && temp < lines2.size(); i++, temp++) 
 			{
 				c[i] = new char[lines2.get(temp).length()];
-				
+
 				for (int j = 0; j < lines2.get(temp).length(); j++)
 				{
 					c[i][j] = lines2.get(temp).charAt(j);
@@ -229,9 +237,9 @@ public class DataToArray {
 			}
 			chars.add(c);
 		}
-		
-		
-/*		for (int i = 0; i < chars.size(); i++) 
+
+
+		/*		for (int i = 0; i < chars.size(); i++) 
 		{
 			for (int j = 0; j < chars.get(i).length; j++) 
 			{
@@ -241,60 +249,60 @@ public class DataToArray {
 				}
 			}
 		}*/
-		
-		
-		
-	//	for (int t = 0; t < chars.size(); t++) // Check every element in the
-	//		// cars and split them up as
-	//		// needed
-	//	{
-	//		boolean alreadyBottomed = true;
-	//		char[][] d = new char[6][100]; // Make it as long
-	//		// as the old
-	//		// element, and
-	//		// we'll trim it
-	//		// later
-	//		int w=0;
-	//		for (int v = 0; v < chars.get(t)[w].length; v++) // Read every
-	//			// column
-	//		{
-	//
-	//			for (w=w ;w < chars.get(t).length; w++) // Then read every row
-	//			{	
-	//				
-	//				char currentChar = chars.get(t)[w][v];
-	//				//System.out.println(chars.get(t)[w].length);
-	//
-	//				 System.out.println("w: " + w + " v: " + v);
-	//				d[w][v] = currentChar;
-	//				if (w == 5 && currentChar == '|' && !alreadyBottomed) 
-	//				{
-	//					v--; // The last column should be printed in twice, so
-	//					// back up one and do this column again
-	//					newchars.add(d); // Add the new element to the list
-	//					d = new char[6][chars.get(t)[w].length]; // Reset the
-	//					// array we
-	//					// are
-	//					// writing
-	//					// so it is
-	//					// blank
-	//					alreadyBottomed = true;
-	//				}
-	//				if (w == 5 && currentChar != '|' && alreadyBottomed) 
-	//				{
-	//					alreadyBottomed = false;
-	//				}
-	//			}
-	//		}
-	//		/*
-	//			if (alreadyBottomed)
-	//			{
-	//				newchars.add(d);
-	//				d = new char[6][chars.get(t)[0].length];
-	//			}
-	//		 */
-	//	}
-	//
+
+
+
+		//	for (int t = 0; t < chars.size(); t++) // Check every element in the
+		//		// cars and split them up as
+		//		// needed
+		//	{
+		//		boolean alreadyBottomed = true;
+		//		char[][] d = new char[6][100]; // Make it as long
+		//		// as the old
+		//		// element, and
+		//		// we'll trim it
+		//		// later
+		//		int w=0;
+		//		for (int v = 0; v < chars.get(t)[w].length; v++) // Read every
+		//			// column
+		//		{
+		//
+		//			for (w=w ;w < chars.get(t).length; w++) // Then read every row
+		//			{	
+		//				
+		//				char currentChar = chars.get(t)[w][v];
+		//				//System.out.println(chars.get(t)[w].length);
+		//
+		//				 System.out.println("w: " + w + " v: " + v);
+		//				d[w][v] = currentChar;
+		//				if (w == 5 && currentChar == '|' && !alreadyBottomed) 
+		//				{
+		//					v--; // The last column should be printed in twice, so
+		//					// back up one and do this column again
+		//					newchars.add(d); // Add the new element to the list
+		//					d = new char[6][chars.get(t)[w].length]; // Reset the
+		//					// array we
+		//					// are
+		//					// writing
+		//					// so it is
+		//					// blank
+		//					alreadyBottomed = true;
+		//				}
+		//				if (w == 5 && currentChar != '|' && alreadyBottomed) 
+		//				{
+		//					alreadyBottomed = false;
+		//				}
+		//			}
+		//		}
+		//		/*
+		//			if (alreadyBottomed)
+		//			{
+		//				newchars.add(d);
+		//				d = new char[6][chars.get(t)[0].length];
+		//			}
+		//		 */
+		//	}
+		//
 		//		ArrayList<char[][]> finalChars = new ArrayList<char[][]>();
 		//
 		//		for (int p = 0; p < newchars.size(); p++) {
@@ -306,21 +314,21 @@ public class DataToArray {
 		//			// characters and be
 		//			// the proper length
 		//		}
-	
+
 		// Test to see printed lines
-		for (int i = 0; i < chars.size(); i++) {
-			System.out.println(Arrays.deepToString(chars.get(i)));
-		}
-	
+		//		for (int i = 0; i < chars.size(); i++) {
+		//			System.out.println(Arrays.deepToString(chars.get(i)));
+		//		}
+		//	
 		// Test to see if characters properly placed in 2-d array.
 		for (int i = 0; i < newchars.size(); i++) {
 			//] System.out.println(Arrays.deepToString(newchars.get(i)));
 		}
-	
+
 		return chars;
 	}
-	
-	
+
+
 	public static void LengthOfPartition() {
 		int length;
 		String temp;
@@ -335,45 +343,46 @@ public class DataToArray {
 			}
 		}
 	}
-	
-	
-	
+
+
+
 	public static int getMaxColumnAmount() {
 		return DataToArray.col;
 	}
-	
+
 	public static int getTotalRowAmount() {
 		return DataToArray.lines.size();
 	}
-	
+
 	public static int getBarAmount() {
 		return DataToArray.lines.size() / 6;
 	}
-	
+
 	public static String getTitle() {
 		return Title;
 	}
-	
+
 	public static String getsubTitle() {
 		return SubTitle;
 	}
-	
+
 	public static float getSpacing() {
 		return Spacing;
 	}
-	
+
+
 	public static void main(String[] args) throws DocumentException,
 	IOException {
 		File file[] = {new File("ExtraWhiteSpaces.txt")};
 		File file2[] = {new File("GarbageInLine.txt")};
-		File file3[] = {new File("WhitespaceInLine.txt")};
+		File file3[] = {new File("elnegrito.txt")};
 		File file4[] = {new File("UnevenLines.txt")};
-//		DataToArray.textToArray(file);
-//		System.out.println("");
-//		DataToArray.textToArray(file2);
-//		System.out.println("");
-//		DataToArray.textToArray(file3);
-//		System.out.println("");
+		//		DataToArray.textToArray(file);
+		//		System.out.println("");
+		//		DataToArray.textToArray(file2);
+		//		System.out.println("");
+		//		DataToArray.textToArray(file3);
+		//		System.out.println("");
 		DataToArray.textToArray(file3);
 		// textToArray();
 		// DataToArray.textToArray(DataToArray.textFile);
@@ -403,6 +412,38 @@ public class DataToArray {
 		}
 		return list;
 	}
+	private static ArrayList<String> ProperLines(ArrayList<String> lines){
+		ArrayList<String> a = new ArrayList<String>();
+		boolean pack =true;
+		try{
+			for(int i=0;i<lines.size();i++){
+				pack =true;
+				if(lines.get(i).length() == lines.get(i+1).length()){
+					for(int j=i;j < i+6;j++){
+						if(!(lines.get(i).length() == lines.get(j).length())){
+							pack = false;
+						//	System.out.println("pack= "+pack);
+							break;
+						}
+					}
+					if(pack){
+						for(int z=i;z<i+6;z++){
+							a.add(lines.get(z));
+
+						}i=i+5;
+					}else{
+						continue;
+					}
+				}else{
+					continue;
+				}
+			}
+		}catch(IndexOutOfBoundsException e){
+			return a;
+		}
+		return a;
+	}
+
 	public static int getLargestNumber(char[][] list)
 	{
 		int max=0;
